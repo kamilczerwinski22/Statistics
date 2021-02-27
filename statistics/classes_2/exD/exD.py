eb89518bc4924c7e5fbbd3b4e5a807338a185b46
Kamil Czerwinski <kamilcze31@gmail.com>
2021-02-27 17:46:02 +0100

    Classes 2 exC update, exD

--- statistics/classes_2/exD/exD.py
+++ statistics/classes_2/exD/exD.py
@@ -1,6 +1,11 @@
-# A script where we have N amount of users and N amount of states. Probability to log in = 0.2, probability to log out
-# = 0.5. We simulate the trajectory - at each iteration each user have a change to be logged in or logged out (
-# depending on users current state). We need to construct convergence graph.
+# A script where we have N amount of users and N amount of states (same as exC but with different logging probability).
+# Probabilities:
+# Log in = 0.2
+# Stay logged out = 0.8
+# Log out = 1 - (0.008 * current state + 0.1)
+# Stay logged in = 0.008 * current state + 0.1
+# We simulate the trajectory - at each iteration each user have a change to be logged in or logged out (depending on
+# users current state). We need to construct convergence graph.
 # Values of Pi show the pursuit of a value to a stationary state.
 # e.g.: At first we have state 0, because all users are logged out. After one iteration 5 users logged in - current
 # state is 5, and so on. On the graph we show can show selected states.
@@ -23,16 +28,19 @@ def change_state(users_list: list, iterations: int, x_to_track: int, leap: int) 
     status_dic: dict = {x: 0 for x in range(len(users_list) + 1)}  # collecting number of occurrences of a given state
     chances_population: list = [0, 1]  # list for tracking if each user state: 0 - logged out, 1 - logged in
     chances_not_logged: list = [0.8, 0.2]  # probabilities to [stay logged out, log in] when user is not logged in
-    chances_logged: list = [0.5, 0.5]  # probabilities to [stay logged in, log out] when user is logged in
     results: list = []
 
     # main function logic
     for iteration in range(1, iterations + 1):
+        current_x = sum(users_list)
+        current_login_chance = 0.008 * current_x + 0.1
+        current_logout_chance = 1 - current_login_chance
+        chances_logged_weights = [current_logout_chance, current_login_chance]
         for idx, user in enumerate(users_list):
             if user == 0:
                 users_list[idx] = random.choices(chances_population, chances_not_logged)[0]
             else:
-                users_list[idx] = random.choices(chances_population, chances_logged)[0]
+                users_list[idx] = random.choices(chances_population, chances_logged_weights)[0]
         status_dic[sum(users_list)] += 1  # sum number of 1's in list - number of logged in users (current state)
         if iteration % leap == 0:
             results.append(status_dic[x_to_track] / iteration)
@@ -63,7 +71,7 @@ def show_graph(num_of_iterations: int, users: list, leap: int, xs_to_track: tupl
     plt.ylabel(f"Experimental Pi values")
     plt.xlabel(f"Number of iterations: {num_of_iterations}, leap every: {leap}")
     plt.title(f"Markov matrix graph example experimental - {len(users)} users/states", loc='left')
-    plt.legend()
+    plt.legend(loc='upper right')
     plt.savefig(f"example1.png")
     plt.show()
 
@@ -77,7 +85,7 @@ def main() -> None:
     users: list = [0 for _ in range(1, 101)]
     iterations: int = 10000
     leap: int = 10
-    xs_to_track: tuple = (29, 31, 33, 35)
+    xs_to_track: tuple = (20, 25, 30, 35)
 
     # functions calling
     show_graph(num_of_iterations=iterations,
